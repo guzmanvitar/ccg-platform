12cdbe4d28dce5fee46f0fde14c72b3024383abf
Guzman VItar <guzmanvitar@gmail.com>
2025-06-15 12:43:37 -0300

    Apply black

--- inference/urls.py
+++ inference/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('panthera-onca/', views.jaguar_tools, name='jaguar_tools'),
+    path("", views.home, name="home"),
+    path("panthera-onca/", views.jaguar_tools, name="jaguar_tools"),
 ]
--- inference/views.py
+++ inference/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
+
 def home(request):
-    return render(request, 'inference/home.html')
+    return render(request, "inference/home.html")
+
 
 def jaguar_tools(request):
-    return render(request, 'inference/jaguar_tools.html')
+    return render(request, "inference/jaguar_tools.html")
