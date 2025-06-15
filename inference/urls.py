25de8d68051c4d3024027212b9215fd16f030fc5
Guzman VItar <guzmanvitar@gmail.com>
2025-06-15 12:42:19 -0300

    Add jaguar page

--- inference/urls.py
+++ inference/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('panthera-onca/', views.jaguar_tools, name='jaguar_tools'),
 ]
--- inference/views.py
+++ inference/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'inference/home.html')
+
+def jaguar_tools(request):
+    return render(request, 'inference/jaguar_tools.html')
