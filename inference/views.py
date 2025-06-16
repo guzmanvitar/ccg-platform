51c02f2a54a76f6e74d363597e34ff4f73e310b1
guzmanvitar <guzmanvitar@gmail.com>
2025-06-16 17:54:49 -0300

    Add form button

--- inference/views.py
+++ inference/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import UploadFileForm
+from django.urls import reverse
 
 def home(request):
     return render(request, "inference/home.html")
@@ -7,3 +8,22 @@ def home(request):
 
 def jaguar_tools(request):
     return render(request, "inference/jaguar_tools.html")
+
+
+def upload_data(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            file = request.FILES['file']
+            # Here we'll later upload to Seafile
+            handle_uploaded_file(file)  # Local stub
+            return redirect(reverse('jaguar_tools'))
+    else:
+        form = UploadFileForm()
+    return redirect(reverse('jaguar_tools'))
+
+
+def handle_uploaded_file(file):
+    with open(f'/tmp/{file.name}', 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
