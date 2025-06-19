b054a72dae4036a9b04db22fbd93a237f0817081
Guzman VItar <guzmanvitar@gmail.com>
2025-06-19 11:24:51 -0300

    Formatting

--- inference/urls.py
+++ inference/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("panthera-onca/", views.jaguar_tools, name="jaguar_tools"),
-    path('upload/', views.upload_data, name='upload_data'),
+    path("upload/", views.upload_data, name="upload_data"),
 ]
